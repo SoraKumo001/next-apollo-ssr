c406b517ec62ab079cf65390bfd92d1f5c36b7f1
SoraKumo <info@croud.jp>
2022-12-30 16:55:50 +0900

    update apollo-server

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,23 +1,47 @@
-import { ApolloServer } from 'apollo-server-micro';
+import { ApolloServer } from '@apollo/server';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { typeDefs } from '../../graphql/typeDefs';
 import { resolvers } from '../../graphql/resolvers';
+import { createGraphQLRequest, createHeaders, createSearch } from '../../libs/apollo-tools';
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
-const startServer = apolloServer.start();
+apolloServer.start();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await startServer;
-  await apolloServer.createHandler({
-    path: req.url,
-  })(req, res);
-}
+/**
+ * Next.js用APIRouteハンドラ
+ */
+const apolloHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  //NextApiRequestをGraphQL用のbody形式に変換(multipart/form-data対応)
+  const [body, removeFiles] = await createGraphQLRequest(req);
+  try {
+    const result = await apolloServer.executeHTTPGraphQLRequest({
+      httpGraphQLRequest: {
+        method: req.method ?? '',
+        headers: createHeaders(req),
+        search: createSearch(req),
+        body,
+      },
+      context: async () => ({ req, res }),
+    });
+    if (result.body.kind === 'complete') {
+      res.end(result.body.string);
+    } else {
+      for await (const chunk of result.body.asyncIterator) {
+        res.write(chunk);
+      }
+      res.end();
+    }
+  } finally {
+    // multipart/form-dataで作成された一時ファイルの削除
+    removeFiles();
+  }
+};
+
+export default apolloHandler;
 
 export const config = {
   api: {
