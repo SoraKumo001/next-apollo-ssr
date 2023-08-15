4897cd4ca15577692fce538ed1addccf8a7a3c0e
SoraKumo <info@croud.jp>
2023-08-16 08:52:57 +0900

    Change package to ESM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,30 @@
 import type { AppType } from 'next/app';
-import { Suspense } from 'react';
+import { useState } from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { SSRProvider } from '../libs/apollo-ssr';
+import { ApolloSSRProvider } from '@react-libraries/apollo-ssr';
 
 const uri = 'https://graphql.anilist.co';
 
-const App: AppType = ({ Component, pageProps }) => {
-  const client = new ApolloClient({
-    uri,
-    cache: new InMemoryCache({}),
-  });
-
+const App: AppType = ({ Component }) => {
+  const [client] = useState(
+    () =>
+      new ApolloClient({
+        uri,
+        cache: new InMemoryCache({}),
+      })
+  );
   return (
     <ApolloProvider client={client}>
-      <SSRProvider>
-        <Suspense fallback={'Loading'}>
-          <Component {...pageProps} />
-        </Suspense>
-      </SSRProvider>
+      {/* ←Add this */}
+      <ApolloSSRProvider>
+        <Component />
+      </ApolloSSRProvider>
     </ApolloProvider>
   );
 };
 
 // getInitialProps itself is not needed, but it is needed to prevent optimization of _app.tsx
 // If you don't include this, it will be executed at build time and will not be called after that.
-App.getInitialProps = () => ({ pageProps: {} });
+App.getInitialProps = () => ({});
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,65 @@
 import { gql, useApolloClient, useSuspenseQuery } from '@apollo/client';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Suspense } from 'react';
 
 // Retrieving the animation list
 const QUERY = gql`
-  {
-    Page {
+  query Query($page: Int, $perPage: Int) {
+    Page(page: $page, perPage: $perPage) {
       media {
-        siteUrl
+        id
         title {
           english
           native
         }
-        description
+      }
+      pageInfo {
+        currentPage
+        hasNextPage
+        lastPage
+        perPage
+        total
       }
     }
   }
 `;
 
 type PageData = {
-  Page: { media: { siteUrl: string; title: { english: string; native: string } }[] };
+  Page: {
+    media: { id: number; siteUrl: string; title: { english: string; native: string } }[];
+    pageInfo: {
+      currentPage: number;
+      hasNextPage: boolean;
+      lastPage: number;
+      perPage: number;
+      total: number;
+    };
+  };
 };
 
-const Page = () => {
-  const { data, refetch } = useSuspenseQuery<PageData>(QUERY);
+const AnimationList = ({ page }: { page: number }) => {
   const client = useApolloClient();
+  const { data, refetch } = useSuspenseQuery<PageData>(QUERY, {
+    variables: { page, perPage: 10 },
+  });
+  const { currentPage, lastPage } = data?.Page?.pageInfo ?? {};
   return (
     <>
-      <button onClick={() => refetch()}>Reload</button>
+      <button onClick={() => refetch()}>Refetch</button>
       <button onClick={() => client.resetStore()}>Reset</button>
+      <div>
+        <Link href={`/?page=${currentPage - 1}`}>
+          <button disabled={currentPage <= 1}>←</button>
+        </Link>
+        <Link href={`/?page=${currentPage + 1}`}>
+          <button disabled={currentPage >= lastPage}>→</button>
+        </Link>
+        {currentPage}/{lastPage}
+      </div>
       {data.Page.media.map((v) => (
         <div
-          key={v.siteUrl}
+          key={v.id}
           style={{ border: 'solid 1px', padding: '8px', margin: '8px', borderRadius: '4px' }}
         >
           <div>
@@ -38,8 +68,18 @@ const Page = () => {
           <a href={v.siteUrl}>{v.siteUrl}</a>
         </div>
       ))}
-      <div></div>
     </>
+  );
+};
+
+const Page = () => {
+  const router = useRouter();
+  const page = Number(router.query.page) || 1;
+
+  return (
+    <Suspense fallback={<div>Loading</div>}>
+      <AnimationList page={page} />
+    </Suspense>
   );
 };
 
